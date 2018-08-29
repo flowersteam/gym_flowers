0aa16f7d73aa090fc52832a8fb8f76841b07c26c
Cédric Colas <cedric.colas@inria.fr>
2018-08-29 11:24:59 +0200

    add armstickball env

--- gym_flowers/__init__.py
+++ gym_flowers/__init__.py
@@ -27,6 +27,22 @@ for mod in modules:
             reward_threshold=1.0,
         )
 
+
+random_objects = [True, False]
+for ro in random_objects:
+    if ro:
+        suffix='ro'
+    else:
+        suffix=''
+    kwargs = dict(random_objects=ro)
+    register(
+        id='ArmStickBall{}-v0'.format(suffix),
+        entry_point='gym_flowers.envs:ArmStickBallV0',
+        max_episode_steps=50,
+        kwargs=kwargs,
+        reward_threshold=1.0,
+    )
+
 arm_lengths = [[0.5, 0.5], [0.5, 0.3, 0.2], [0.4, 0.3, 0.2, 0.1], [0.35, 0.25, 0.2, 0.1, 0.1],  [0.3, 0.2, 0.2, 0.1, 0.1, 0.05], [0.3, 0.2, 0.2, 0.1, 0.1, 0.05, 0.05]]
 for i in range(2,7):
     suffix=str(i)
