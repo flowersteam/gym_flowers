2522468592a53f2265d36600788fe630c3792e82
Cédric Colas <cedric.colas@inria.fr>
2018-09-21 12:41:23 +0200

    3D above cubo0

--- gym_flowers/envs/robotics/fetch_env_modular.py
+++ gym_flowers/envs/robotics/fetch_env_modular.py
@@ -47,10 +47,10 @@ class ModularFetchEnv(robot_env_modular.ModularRobotEnv):
 
         # define the different tasks
         # task 0: Hand position (3D)
-        # task 1: Cube0 position (2D)
+        # task 1: Cube1 position (2D)
         # task 2: Cube2 position (2D)
         # task 3: Cube1 position (3D above Cube0)
-        # task 4: Stack Cube0 and Cube1 in given position (2D)
+        # task 4: Stack Cube1 over Cube0 in given position (2D)
         self.tasks = tasks
         self.n_tasks = len(self.tasks)
         self.tasks_obs_id = [[0, 1, 2], [6, 7, 8], [9, 10, 11], [6, 7, 8], [6, 7, 8, 0, 1, 2]]
@@ -206,11 +206,11 @@ class ModularFetchEnv(robot_env_modular.ModularRobotEnv):
                 goal_to_render = tmp_goal.copy()
 
             elif t == 3:  # 3D coordinates for the object
-                tmp_goal = self.initial_gripper_xpos[:3] + goal * self.target_range + self.target_offset
-                tmp_goal[2] = self.height_offset + (goal[2] + 1) * 0.45 / 2  # mapping in -1,1 to 0,0.45 #self.np_random.uniform(0, 0.45)
-                # obs = self._get_obs()
-                # tmp_goal = obs['observation'][6:9].copy()
-                # tmp_goal[2] = self.height_offset + (goal[2] + 1.2) * 0.45 / 2.2  # mapping in -1,1 to 0,0.45 #self.np_random.uniform(0, 0.45)
+                # tmp_goal = self.initial_gripper_xpos[:3] + goal * self.target_range + self.target_offset
+                # tmp_goal[2] = self.height_offset + (goal[2] + 1) * 0.45 / 2  # mapping in -1,1 to 0,0.45 #self.np_random.uniform(0, 0.45)
+                obs = self._get_obs()
+                tmp_goal = obs['observation'][3:6].copy()
+                tmp_goal[2] = self.height_offset + (goal[2] + 1.2) * 0.45 / 2.2  # mapping in -1,1 to 0,0.45 #self.np_random.uniform(0, 0.45)
                 desired_goal[self.tasks_g_id[t]] = tmp_goal.copy()
                 goal_to_render = tmp_goal.copy()
 
