8191c00eae43221b1138079b2c4c36d5207e3bb2
Cédric Colas <cedric.colas@inria.fr>
2018-10-05 10:30:40 +0200

    1 dist in obs, 1 real

--- gym_flowers/envs/robotics/fetch_env_modular.py
+++ gym_flowers/envs/robotics/fetch_env_modular.py
@@ -419,7 +419,7 @@ class ModularFetchEnv(robot_env_modular.ModularRobotEnv):
                                   grip_velp, gripper_vel, gripper_state])
         elif self.n_tasks == 5:
             obs = np.concatenate([grip_pos,
-                                  object0_pos.ravel(), object1_pos.ravel(), object2_pos.ravel(),  self.object3_xpos.squeeze(), self.object4_xpos.squeeze(), # object3_pos.ravel(),
+                                  object0_pos.ravel(), object1_pos.ravel(), object2_pos.ravel(),  #self.object3_xpos.squeeze(), self.object4_xpos.squeeze(), # object3_pos.ravel(),
                                  # object4_pos.ravel(),
                                   object0_rel_pos.ravel(), object1_rel_pos.ravel(), object2_rel_pos.ravel(),  # object3_rel_pos.ravel(), object4_rel_pos.ravel(),
                                   object0_rot.ravel(), object1_rot.ravel(), object2_rot.ravel(),  # object3_rot.ravel(), object4_rot.ravel(),
