cef9da04a8f94b120550a687b9d112f7b8ec4d22
Cédric Colas <cedric.colas@inria.fr>
2018-11-13 11:39:17 +0100

    commit just to change branch

--- gym_flowers/envs/rooms/test_env.py
+++ gym_flowers/envs/rooms/test_env.py
@@ -2,14 +2,15 @@ import numpy as np
 import gym
 import gym_flowers
 
-env = gym.make('Rooms-v0')
+env = gym.make('Rooms5-v0')
 env.reset()
-task = 2
+task = 1
 goal = np.random.uniform(-1,1,2)
+goal = np.array([0,1])
 env.unwrapped.reset_task_goal(task=task, goal=goal)
 env.render()
-for i in range(500):
-    act = np.array([1,-1,1])
+while True:
+    act = np.array([0,0,0])
     obs = env.step(act)
-    # print(obs[1])
+    print(obs[1])
     env.render()
